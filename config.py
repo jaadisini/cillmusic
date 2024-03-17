06d5cb7d488c6a925230dcd08c21c74650514a69
jaadisini <162589971+jaadisini@users.noreply.github.com>
2024-03-18 00:29:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/stayheresat")
+    "SUPPORT_CHANNEL", "https://t.me/stayheresay")
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", "https://t.me/cillsupport")
 

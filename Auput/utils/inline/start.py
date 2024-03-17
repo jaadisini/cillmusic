6a7ac316c24a0b9b43e9e33e444993db760396f2
jaadisini <162589971+jaadisini@users.noreply.github.com>
2024-03-18 02:21:38 +0700

    Update start.py

--- Auput/utils/inline/start.py
+++ Auput/utils/inline/start.py
@@ -98,7 +98,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
                 InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"https://github.com/jaadisini/cillmusic"
+                    text=_["S_B_6"], url=f"https://xnxx.com"
                 ),
             ]
         )
@@ -107,7 +107,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text=_["S_B_6"], url=f"https://github.com/jaadisini/cillmusic"
+                        text=_["S_B_6"], url=f"https://xnxx.com"
                     ),
                 ]
             )

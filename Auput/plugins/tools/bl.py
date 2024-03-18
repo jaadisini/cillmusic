380bfc1ceb138082783e95fa0a078b9e878527a3
jaadisini <162589971+jaadisini@users.noreply.github.com>
2024-03-18 19:39:00 +0700

    Update bl.py

--- Auput/plugins/tools/bl.py
+++ Auput/plugins/tools/bl.py
@@ -23,8 +23,8 @@ from Auput.utils.filter_group import blacklist_filters_group
 
 @app.on_message(filters.command("bl") & ~filters.private)
 @adminsOnly("can_restrict_members")
-async def save_filters(_, message):
-
+async def save_filters(_, message: Message):
+    user = message.from_user
     admin_list = await list_admins(message.chat.id)
     if user.id not in admin_list:
         return

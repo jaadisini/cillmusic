7e1a6953063ca5701a43ea37ae92ab8ae887c24e
jaadisini <162589971+jaadisini@users.noreply.github.com>
2024-03-21 11:35:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ UPSTREAM_REPO = getenv(
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 # GIT TOKEN ( if your edited repo is private)
-GIT_TOKEN = getenv("GIT_TOKEN", None)
+GIT_TOKEN = getenv("GIT_TOKEN", "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")
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
